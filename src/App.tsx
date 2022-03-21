6651c4b44c7b66bcef3eb5fa5ee59dffff3c0d4d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-21 12:55:54 +0100

    adding clouds and stars

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import * as THREE from "three";
-
-import { OrbitControls, Sky, Stars } from "@react-three/drei";
-import { Suspense, useRef } from "react";
-
 import { Canvas } from "@react-three/fiber";
 import Earth from "./components/earth";
+import { Stars } from "@react-three/drei";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <Canvas>
+    <Canvas style={{ background: "black" }}>
       <Suspense fallback={null}>
-        <OrbitControls />
         <Earth />
+        <Stars fade />
       </Suspense>
     </Canvas>
   );
--- src/components/earth/index.tsx
+++ src/components/earth/index.tsx
@@ -1,8 +1,11 @@
+import * as THREE from "three";
+
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNightMap from "../../assets/textures/8k_earth_nightmap.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import { FC } from "react";
+import { OrbitControls } from "@react-three/drei";
 import { TextureLoader } from "three";
 import { useLoader } from "@react-three/fiber";
 
@@ -13,11 +16,29 @@ const Earth: FC = () => {
   );
   return (
     <>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1} />
+      <mesh>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite
+          transparent
+          side={THREE.DoubleSide}
+        />
+      </mesh>
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <OrbitControls
+          enableZoom
+          enablePan
+          enableRotate
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
       </mesh>
     </>
   );
